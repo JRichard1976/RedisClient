c7e95cd647a15bb8e345b70dc3bd79c09d3c25f2
JRichard1976 <115026751+JRichard1976@users.noreply.github.com>
2022-10-05 07:47:54 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using RedisClient;
+using StackExchange.Redis;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+internal  class Program
+{
+    static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+                 new ConfigurationOptions
+                 {
+                     EndPoints = { "localhost:6379" }
+                 });
+    static async Task Main(string[] args)
+    {
+        //var test =  await TexCache.Connect();
+        string EXPIRED_KEYS_CHANNEL = "__keyevent@0__:*";
+        ISubscriber subscriber = redis.GetSubscriber();
+        subscriber.Subscribe(EXPIRED_KEYS_CHANNEL, async (channel, key) =>
+        {
+            var Res = await TexCacheHelpers.GetValue($"{key}");
+            Console.WriteLine($"{channel}: {key} {Res}");
+            
+        }
+        );
+        Console.WriteLine("Listening for events...");
+        for (int K=0; K<10; K++)
+        {
+            await TexCacheHelpers.SetValue("Bassetti2.Tes." + K, "Test3" + K);
+            
+            
+            var Res = await TexCacheHelpers.GetValue("Bassetti.Tes." + K.ToString());
+
+            if (Res == "")
+                Console.WriteLine("Don't exist");
+            else
+                Console.WriteLine(Res.ToString());
+
+        }
+        
+        Console.ReadKey();
+    }
+   
+}
---
+++ TexCacheHelpers.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System.Threading.Channels;
+
+namespace RedisClient
+{
+    internal static class TexCacheHelpers
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+                new ConfigurationOptions
+                {
+                    EndPoints = { "localhost:6379" }
+                });
+        
+        
+        public static Task SetValue(string Key, string Value)
+        {
+            var db = redis.GetDatabase();
+            db.StringSet(Key, Value);
+            return Task.CompletedTask;
+
+        }
+        public static Task<string> GetValue(string Key)
+        {
+            var db = redis.GetDatabase();
+            var res = db.StringGet (Key);
+            return Task.FromResult(res.ToString());
+
+        }
+    }
+}
